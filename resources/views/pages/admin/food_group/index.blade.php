3b85ddd56889ff252db7838943fe45caa47436fd
Aburiza41 <aburiza.41@student.untan.ac.id>
2024-02-01 22:14:14 +0700

    Modify
    Perubahan tampilkan index kelompok makanan pada admin

--- resources/views/pages/admin/food_group/index.blade.php
+++ resources/views/pages/admin/food_group/index.blade.php
@@ -34,26 +34,26 @@
                               <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                    <tr>
                                         <th scope="col" class="px-6 py-3">
-                                             Food Group
+                                             Nama
                                         </th>
                                         <th scope="col" class="px-6 py-3">
-                                             Keterangan
+                                             Kode Kelompok Makanan
                                         </th>
                                         <th scope="col" class="px-6 py-3">
-                                             Action
+                                             Aksi
                                         </th>
                                    </tr>
                               </thead>
                               <tfoot class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                    <tr>
                                         <th scope="col" class="px-6 py-3">
-                                             Food Group
+                                             Nama
                                         </th>
                                         <th scope="col" class="px-6 py-3">
-                                             Food Name
+                                             Kode Kelompok Makanan
                                         </th>
                                         <th scope="col" class="px-6 py-3">
-                                             Action
+                                             Aksi
                                         </th>
                                    </tr>
                               </tfoot>
